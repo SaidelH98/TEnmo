747c2626ae610825562e7d2b7cf41bcedad1eb97
Saidel Halol <saidelhalol98@gmail.com>
2023-07-05 09:39:42 -0700

    BTController

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/BalanceTransferController.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/BalanceTransferController.java
@@ -1,6 +1,24 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.dao.UserDao;
+import com.techelevator.tenmo.security.jwt.TokenProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BalanceTransferController {
+
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final UserDao userDao;
+
+    public BalanceTransferController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder, UserDao userDao) {
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.userDao = userDao;
+    }
 
 
 
